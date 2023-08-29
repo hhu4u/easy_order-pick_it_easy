065a416bc38b17dc6818cab19b21d357c5fae82d
herve hublet <hervehublet@hotmail.com>
2023-08-29 12:19:52 +0200

    Products controller started

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,42 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.where(restaurant_id: params[:restaurant_id])
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @product = Product.new(product_params)
+    @product.restaurant = @restaurant
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price)
+  end
+end
