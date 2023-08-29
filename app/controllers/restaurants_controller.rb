84cc9305ef2bc81f6b818d4508bc90bb229ab95d
Marianna Rekoumi <marianna_rek@hotmail.com>
2023-08-29 12:15:47 +0200

    implement restaurant create/update/show/index

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,12 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update]
+  def index
+    @restaurants = Restaurant.where(user: current_user)
+  end
+
+  def show
+  end
+
   def new
     @restaurant = Restaurant.new
   end
@@ -13,9 +21,21 @@ class RestaurantsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @restaurant.update(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
   private
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :address)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,5 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
+  has_many :products
+  validates :name, :address, presence: true
 end
