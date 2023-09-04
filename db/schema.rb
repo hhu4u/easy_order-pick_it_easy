21ce7e211909d0a2d86e6e1fe2c31ea99e8100cc
Marianna Rekoumi <marianna_rek@hotmail.com>
2023-09-04 15:44:31 +0200

    generate qr code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_31_104125) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_04_102249) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -92,6 +92,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_31_104125) do
     t.bigint "restaurant_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "qr_code"
     t.index ["restaurant_id"], name: "index_tables_on_restaurant_id"
   end
 
