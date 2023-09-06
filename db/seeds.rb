ba18090a247ee053df4b37321e93ec1a665911fe
herve hublet <hervehublet@hotmail.com>
2023-09-06 16:31:24 +0200

    seed basket inversion correction

--- db/seeds.rb
+++ db/seeds.rb
@@ -340,14 +340,14 @@ product21.save
 
 puts "Creating owner1 basket"
 basket1 = Basket.new
-basket1.table = owner1.restaurants.first.tables.first
+basket1.table = owner2.restaurants.first.tables.first
 basket1.basket_status = "New"
 basket1.user = owner1
 basket1.save
 
 puts "Creating owner2 basket"
 basket2 = Basket.new
-basket2.table = owner2.restaurants.first.tables.first
+basket2.table = owner1.restaurants.first.tables.first
 basket2.basket_status = "New"
 basket2.user = owner2
 basket2.save
