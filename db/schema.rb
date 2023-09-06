d1b4120948045563ab6499e3fd9ca4226972cc91
herve hublet <hervehublet@hotmail.com>
2023-09-06 12:56:39 +0200

    Migration add available to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_04_102249) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_06_104334) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,6 +76,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_04_102249) do
     t.datetime "updated_at", null: false
     t.string "dish_type"
     t.string "ingredients"
+    t.boolean "available", default: true
     t.index ["restaurant_id"], name: "index_products_on_restaurant_id"
   end
 
