4c981a2860105d39a5ecc9937274b232e1b6d47f
herve hublet <hervehublet@hotmail.com>
2023-09-06 10:43:45 +0200

    Stripe payment update

---
+++ app/services/stripe_checkout_session_service.rb
@@ -0,0 +1,9 @@
+class StripeCheckoutSessionService
+  def call(event)
+    basket = Basket.find_by(checkout_session_id: event.data.object.id)
+    basket.update(payment_status: 'paid')
+
+    puts "---------------------------------------------------------------"
+    puts "payment updated"
+  end
+end
